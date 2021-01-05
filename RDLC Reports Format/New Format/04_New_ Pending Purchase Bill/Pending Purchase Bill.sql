52af04245edb29ebded7e72df2f9487462c81f81
sajalde <33744740+sajalmgts@users.noreply.github.com>
2021-01-05 21:56:25 +0530

    Purchase bill

--- RDLC Reports Format/New Format/04_New_ Pending Purchase Bill/Pending Purchase Bill.sql
+++ RDLC Reports Format/New Format/04_New_ Pending Purchase Bill/Pending Purchase Bill.sql
@@ -14,6 +14,8 @@ IF OBJECT_ID('tempdb..#tmpCompanyID') IS NOT NULL DROP TABLE #tmpCompanyID
 IF OBJECT_ID('tempdb..#tmpPartyName') IS NOT NULL DROP TABLE #tmpPartyName
 IF OBJECT_ID('tempdb..#tmpStockGroupName') IS NOT NULL DROP TABLE #tmpStockGroupName
 IF OBJECT_ID('tempdb..#tmpStockItemName') IS NOT NULL DROP TABLE #tmpStockItemName
+IF OBJECT_ID('tempdb..#tempPendingPurchaseBills') IS NOT NULL DROP TABLE #tempPendingPurchaseBills
+
 
 SELECT NAME AS 'CompanyName' INTO #tmpCompanyName  from dbo.GetTableFromString(isnull(@CompanyNames,''))
 Select c.CompanyID Into #tmpCompanyID  From #tmpCompanyName as t Inner Join TD_Mst_Company as c ON c.CompanyName=t.CompanyName
@@ -21,42 +23,39 @@ SELECT NAME AS 'PartyName' INTO #tmpPartyName from dbo.GetTableFromString(isnull
 SELECT NAME AS 'StockGroupName' INTO #tmpStockGroupName from dbo.GetTableFromString(isnull(@StockGroupName_List,''))
 SELECT NAME AS 'StockItemName' INTO #tmpStockItemName from dbo.GetTableFromString(isnull(@StockItemName_List,''))
 
---select * from View_Report_LeadTime as LT
---where  (@CompanyNames  <> '' AND LT.CompanyID IN (SELECT CompanyID FROM #tmpCompanyID)  OR (@CompanyNames = ''  AND LT.CompanyID = LT.CompanyID))
---	And (@PartyName_List  <> '' AND LT.popartyname IN (SELECT PartyName FROM #tmpPartyName)  OR (@PartyName_List = ''  AND LT.popartyname =LT.popartyname))
---	And (@StockItemName_List  <> '' AND LT.POStockItemName IN (SELECT StockItemName FROM #tmpStockItemName)  OR (@StockItemName_List = ''  AND LT.POStockItemName =LT.POStockItemName ))
---Order by podate, popartyname, POStockItemName
 
-SELECT GRN.Date AS [TransDate], GRN.Reference AS [ChallanBillNo], GRN.PartyName AS [VendorName],  GRN.StockItemName AS [StockItemName], '' as [StockGroup], GRN.ActualQuantity AS [QTY], 
-                  GRN.Rate AS [Rate],  GRN.Amount AS [Amount] 
-FROM     (SELECT dbo.TD_Txn_VchHdr.CompanyID, dbo.TD_Txn_VchHdr.Date, dbo.TD_Txn_VchHdr.VoucherTypeName, dbo.TD_Txn_VchHdr.VoucherNo, dbo.TD_Txn_VchHdr.Reference, dbo.TD_Txn_VchHdr.PartyName, dbo.TD_Txn_InvLine.Rate, 
-                                    dbo.TD_Txn_InvLine.StockItemName, dbo.TD_Txn_InvLine.RateUOM, dbo.TD_Txn_InvLine.Amount, dbo.TD_Txn_InvLine.ActualQuantity, dbo.TD_Txn_InvLine.ActualUOM
-                  FROM      dbo.TD_Txn_VchHdr INNER JOIN
-                                    dbo.TD_Txn_InvLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_InvLine.CompanyId AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_InvLine.VoucherID INNER JOIN
-                                    dbo.TD_Txn_AccLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_AccLine.CompanyID AND dbo.TD_Txn_InvLine.AccLineNo = dbo.TD_Txn_AccLine.AccLineNo AND 
-                                    dbo.TD_Txn_InvLine.VoucherID = dbo.TD_Txn_AccLine.VoucherID AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_AccLine.VoucherID
-                  WHERE   (dbo.TD_Txn_VchHdr.CompanyID = 2) AND (dbo.TD_Txn_VchHdr.VoucherTypeName LIKE 'Purchase Order for Factory') OR
-                                    (dbo.TD_Txn_VchHdr.CompanyID = 2) AND (dbo.TD_Txn_VchHdr.VoucherTypeName LIKE 'Purchase Order for Central Hub')) AS PO LEFT OUTER JOIN
-                      (SELECT dbo.TD_Txn_VchHdr.CompanyID, dbo.TD_Txn_VchHdr.Date, dbo.TD_Txn_VchHdr.VoucherTypeName, dbo.TD_Txn_VchHdr.VoucherNo, dbo.TD_Txn_VchHdr.Reference, dbo.TD_Txn_VchHdr.PartyName, 
-                                         dbo.TD_Txn_InvLine.Rate, dbo.TD_Txn_InvLine.StockItemName, dbo.TD_Txn_InvLine.RateUOM, dbo.TD_Txn_InvLine.Amount, dbo.TD_Txn_InvLine.ActualQuantity, dbo.TD_Txn_InvLine.ActualUOM, dbo.TD_Txn_VchHdr.OrderNo, 
-                                         dbo.TD_Txn_VchHdr.OrderDate
-                       FROM      dbo.TD_Txn_VchHdr INNER JOIN
-                                         dbo.TD_Txn_InvLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_InvLine.CompanyId AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_InvLine.VoucherID INNER JOIN
-                                         dbo.TD_Txn_AccLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_AccLine.CompanyID AND dbo.TD_Txn_InvLine.AccLineNo = dbo.TD_Txn_AccLine.AccLineNo AND 
-                                         dbo.TD_Txn_InvLine.VoucherID = dbo.TD_Txn_AccLine.VoucherID AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_AccLine.VoucherID
-                       WHERE   (dbo.TD_Txn_VchHdr.CompanyID = 2) AND (dbo.TD_Txn_VchHdr.VoucherTypeName LIKE 'GRN%')) AS GRN ON PO.Reference = GRN.OrderNo AND PO.StockItemName = GRN.StockItemName LEFT OUTER JOIN
-                      (SELECT dbo.TD_Txn_VchHdr.CompanyID, dbo.TD_Txn_VchHdr.Date, dbo.TD_Txn_VchHdr.VoucherTypeName, dbo.TD_Txn_VchHdr.VoucherNo, dbo.TD_Txn_VchHdr.Reference, dbo.TD_Txn_VchHdr.PartyName, 
-                                         dbo.TD_Txn_InvLine.Rate, dbo.TD_Txn_InvLine.StockItemName, dbo.TD_Txn_InvLine.RateUOM, dbo.TD_Txn_InvLine.Amount, dbo.TD_Txn_InvLine.ActualQuantity, dbo.TD_Txn_InvLine.ActualUOM, 
-                                         dbo.TD_Txn_VchHdr.OrderNo
-                       FROM      dbo.TD_Txn_VchHdr INNER JOIN
-                                         dbo.TD_Txn_InvLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_InvLine.CompanyId AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_InvLine.VoucherID INNER JOIN
-                                         dbo.TD_Txn_AccLine ON dbo.TD_Txn_VchHdr.CompanyID = dbo.TD_Txn_AccLine.CompanyID AND dbo.TD_Txn_InvLine.AccLineNo = dbo.TD_Txn_AccLine.AccLineNo AND 
-                                         dbo.TD_Txn_InvLine.VoucherID = dbo.TD_Txn_AccLine.VoucherID AND dbo.TD_Txn_VchHdr.VoucherID = dbo.TD_Txn_AccLine.VoucherID
-                       WHERE   (dbo.TD_Txn_VchHdr.CompanyID = 2) AND (dbo.TD_Txn_VchHdr.VoucherTypeName LIKE 'PURCHASE FOR%')) AS PURCHASE ON GRN.OrderNo = PURCHASE.OrderNo AND 
-                  GRN.StockItemName = PURCHASE.StockItemName AND GRN.ActualQuantity = PURCHASE.ActualQuantity LEFT OUTER JOIN
-                      (SELECT dbo.TD_Txn_AccLine.LedgerName, dbo.TD_Txn_AccLine.Amount, dbo.TD_Txn_BillLine.BillType, dbo.TD_Txn_BillLine.BillName, dbo.TD_Txn_BillLine.Amount AS Expr1, dbo.TD_Txn_BillLine.Date, 
-                                         dbo.TD_Txn_AccLine.CompanyID
-                       FROM      dbo.TD_Txn_BillLine INNER JOIN
-                                         dbo.TD_Txn_AccLine ON dbo.TD_Txn_BillLine.Companyid = dbo.TD_Txn_AccLine.CompanyID AND dbo.TD_Txn_BillLine.VoucherId = dbo.TD_Txn_AccLine.VoucherID AND 
-                                         dbo.TD_Txn_BillLine.AccLineNo = dbo.TD_Txn_AccLine.AccLineNo
-                       WHERE   (dbo.TD_Txn_BillLine.BillType LIKE 'Agst Ref') AND (dbo.TD_Txn_AccLine.CompanyID = 2)) AS payment ON PURCHASE.Reference = payment.BillName
+SELECT GRN.Date AS [TransDate], GRN.Reference AS [ChallanBillNo], GRN.PartyName AS [VendorName],  GRN.StockItemName AS [StockItemName], GRN.StockGroup  as [StockGroup],
+	   GRN.ActualQuantity AS [GRNQTY], GRN.Rate AS [GRNRate], IsNull(PURCHASE.ActualQuantity, 0) as [BillQTY], IsNull(PURCHASE.Rate, 0) as [BillRate]
+INTO #tempPendingPurchaseBills
+FROM    
+	(
+		SELECT VH.CompanyID, VH.Date, VH.VoucherTypeName, VH.VoucherNo, VH.Reference, VH.PartyName, vh.OrderNo, IL.StockItemName, SI.StockGroup, IL.RateUOM,  IL.ActualUOM	, IL.ActualQuantity, IL.Rate, (IL.ActualQuantity *  IL.Rate) as [Amount]					
+		FROM  TD_Txn_VchHdr as VH
+		INNER JOIN TD_Txn_InvLine as IL ON VH.CompanyID = IL.CompanyId AND VH.VoucherID = IL.VoucherID 
+		INNER JOIN TD_Txn_AccLine as AL ON VH.CompanyID = AL.CompanyID AND IL.AccLineNo = AL.AccLineNo AND IL.VoucherID = AL.VoucherID AND VH.VoucherID = AL.VoucherID
+		Inner Join TD_Mst_StockItem as SI ON SI.StockItemName=IL.StockItemName
+		WHERE  (VH.VoucherTypeName LIKE 'GRN%')
+		And (@CompanyNames  <> '' AND VH.CompanyID IN (SELECT CompanyID FROM #tmpCompanyID)  OR (@CompanyNames = ''  AND VH.CompanyID = VH.CompanyID))
+		And VH.Date >= @DateFrom AND VH.Date <= @DateTo
+	) AS GRN  
+	LEFT OUTER JOIN	
+	(
+		SELECT VH.CompanyID, VH.Date, VH.VoucherTypeName, VH.VoucherNo, VH.Reference, VH.PartyName, IL.Rate, IL.StockItemName, IL.RateUOM, IL.Amount, IL.ActualQuantity, IL.ActualUOM, VH.OrderNo
+		FROM  TD_Txn_VchHdr as VH
+		INNER JOIN TD_Txn_InvLine as IL ON VH.CompanyID = IL.CompanyId AND VH.VoucherID = IL.VoucherID 
+		INNER JOIN TD_Txn_AccLine as AL ON VH.CompanyID = AL.CompanyID AND IL.AccLineNo = AL.AccLineNo AND IL.VoucherID = AL.VoucherID AND VH.VoucherID = AL.VoucherID
+		WHERE  (VH.VoucherTypeName LIKE 'PURCHASE FOR%')
+		And (@CompanyNames  <> '' AND VH.CompanyID IN (SELECT CompanyID FROM #tmpCompanyID)  OR (@CompanyNames = ''  AND VH.CompanyID = VH.CompanyID))
+
+	) AS PURCHASE 
+		ON GRN.OrderNo = PURCHASE.OrderNo AND GRN.StockItemName = PURCHASE.StockItemName AND GRN.ActualQuantity = PURCHASE.ActualQuantity 
+
+
+Select Distinct B.TransDate, B.ChallanBillNo, B.VendorName, B.StockGroup, B.StockItemName, (B.GRNQTY - B.BillQTY) as [QTY], B.GRNRate as [Rate], (B.GRNQTY - B.BillQTY) * B.GRNRate As [Amount]
+
+from #tempPendingPurchaseBills as B
+Where (@PartyName_List  <> '' AND B.VendorName IN (SELECT PartyName FROM #tmpPartyName)  OR (@PartyName_List = ''  AND B.VendorName = B.VendorName ))
+And   (@StockGroupName_List  <> '' AND B.StockGroup IN (SELECT StockGroupName FROM #tmpStockGroupName)  OR (@StockGroupName_List = ''  AND B.StockGroup = B.StockGroup))
+And   (@StockItemName_List  <> '' AND B.StockItemName IN (SELECT StockItemName FROM #tmpStockItemName)  OR (@StockItemName_List = ''  AND B.StockItemName = B.StockItemName))
+And B.GRNQTY > B.BillQTY
+Order by B.TransDate, B.VendorName, B.StockGroup, B.StockItemName Desc
