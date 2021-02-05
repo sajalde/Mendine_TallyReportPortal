e27b61af060d7e9e6f01d7a9590c64a65ffa6bd9
sajalde <33744740+sajalmgts@users.noreply.github.com>
2021-02-05 23:13:04 +0530

    report bug

--- RDLC Reports Format/New Format/02_Godown Stock Transfer -Done/GodownStockReport.sql
+++ RDLC Reports Format/New Format/02_Godown Stock Transfer -Done/GodownStockReport.sql
@@ -1,23 +1,29 @@
 --- Godown Stock Transfer Report ----
---DECLARE @CompanyNames as Varchar(2000)='', @DateFrom datetime= Null,  @DateTo datetime=Null,   @PartyName_List as Varchar(250)='', @StockCategory_List as Varchar(250)='', @StockItemName_List as Varchar(250)=''
+DECLARE @CompanyNames as Varchar(2000)='', @DateFrom datetime= Null,  @DateTo datetime=Null,   @PartyName_List as Varchar(250)='',
+@StockCategory_List as Varchar(2000)='', @StockItemName_List as Varchar(2000)='', @GodownName_Source_List as Varchar(2000)='', @GodownName_Destination_List as Varchar(2000)=''
 
---Set @CompanyNames='Mendine Pharmaceuticals Pvt Ltd. (FY 2019-20)Server'
---SET @DateFrom ='01/01/2020'
---SET @DateTo = '10/30/2020'
---SET @PartyName_List = ''
---SET @StockCategory_List=''
---SET @StockItemName_List=''
+Set @CompanyNames='Mendine Pharmaceuticals Pvt Ltd. (FY 2019-20)Server'
+SET @DateFrom ='01/01/2020'
+SET @DateTo = '10/30/2020'
+SET @PartyName_List = ''
+SET @StockCategory_List=''
+SET @StockItemName_List=''
+SET @GodownName_Source_List=''
+SET @GodownName_Destination_List=''
 
 IF OBJECT_ID('tempdb..#tmpCompanyName') IS NOT NULL DROP TABLE #tmpCompanyName
 IF OBJECT_ID('tempdb..#tmpCompanyID') IS NOT NULL DROP TABLE #tmpCompanyID
 IF OBJECT_ID('tempdb..#tmpStockCategory') IS NOT NULL DROP TABLE #tmpStockCategory
 IF OBJECT_ID('tempdb..#tmpStockItemName') IS NOT NULL DROP TABLE #tmpStockItemName
-
+IF OBJECT_ID('tempdb..#tmpSourceGodownName') IS NOT NULL DROP TABLE #tmpSourceGodownName
+IF OBJECT_ID('tempdb..#tmpDestinationGodownName') IS NOT NULL DROP TABLE #tmpDestinationGodownName
 
 SELECT NAME AS 'CompanyName' INTO #tmpCompanyName  from dbo.GetTableFromString(isnull(@CompanyNames,''))
 Select c.CompanyID Into #tmpCompanyID  From #tmpCompanyName as t Inner Join TD_Mst_Company as c ON c.CompanyName=t.CompanyName
 SELECT NAME AS 'StockCategory' INTO #tmpStockCategory from	 dbo.GetTableFromString(isnull(@StockCategory_List,''))
 SELECT NAME AS 'StockItemName' INTO #tmpStockItemName from dbo.GetTableFromString(isnull(@StockItemName_List,''))
+SELECT NAME AS 'GodownName' INTO #tmpSourceGodownName from dbo.GetTableFromString(isnull(@GodownName_Source_List,''))
+SELECT NAME AS 'GodownName' INTO #tmpDestinationGodownName from dbo.GetTableFromString(isnull(@GodownName_Destination_List,''))
 
 Select Top(100) bl.EntryDate, IL.StockItemName, il.BilledQuantity, il.ActualUOM, si.StockCategory, il.Rate,  bl.Amount, vt.VoucherType
 ,bl.GodownName, BL.DestinationGodownName
@@ -29,20 +35,12 @@ INNER JOIN TD_Mst_VoucherType as VT ON MC.CompanyID = VT.CompanyID
 INNER JOIN TD_Txn_VchHdr as VH ON VT.CompanyID = VH.CompanyID AND  VT.VoucherTypeName = VH.VoucherTypeName 
 INNER JOIN TD_Txn_AccLine as AL ON VH.CompanyID = AL.CompanyID AND VH.VoucherID = AL.VoucherID and IL.CompanyId = AL.CompanyID AND IL.VoucherID = AL.VoucherID AND IL.AccLineNo = AL.AccLineNo
 where (VT.VoucherType ='Stock Journal' OR VT.VoucherType= '#Internal Stock Journal#')
-	AND (VH.IsOptional <> 1 AND VH.IsCancelled <> 1 AND VH.IsDeleted <> 1) AND AL.IsPartyLedger = 0  And MC.CompanyID IN (2) 
-	and bl.EntryDate >= @DateFrom  and bl.EntryDate <= @DateTo
-	And (@CompanyNames  <> '' AND MC.CompanyID IN (SELECT CompanyID FROM #tmpCompanyID)  OR (@CompanyNames = ''  AND MC.CompanyID = MC.CompanyID))
-	And (@StockCategory_List  <> '' AND SI.StockCategory IN (SELECT StockCategory FROM #tmpStockCategory)  OR (@StockCategory_List = ''  AND SI.StockCategory  = SI.StockCategory))
-	And (@StockItemName_List  <> '' AND IL.StockItemName IN (SELECT StockItemName FROM #tmpStockItemName)  OR (@StockItemName_List = ''  AND IL.StockItemName = IL.StockItemName ))
+AND (VH.IsOptional <> 1 AND VH.IsCancelled <> 1 AND VH.IsDeleted <> 1) AND AL.IsPartyLedger = 0  And MC.CompanyID IN (2) 
+and bl.EntryDate >= @DateFrom  and bl.EntryDate <= @DateTo
+And (@CompanyNames  <> '' AND MC.CompanyID IN (SELECT CompanyID FROM #tmpCompanyID)  OR (@CompanyNames = ''  AND MC.CompanyID = MC.CompanyID))
+And (@StockCategory_List  <> '' AND SI.StockCategory IN (SELECT StockCategory FROM #tmpStockCategory)  OR (@StockCategory_List = ''  AND SI.StockCategory  = SI.StockCategory))
+And (@StockItemName_List  <> '' AND IL.StockItemName IN (SELECT StockItemName FROM #tmpStockItemName)  OR (@StockItemName_List = ''  AND IL.StockItemName = IL.StockItemName ))
+And (@GodownName_Source_List  <> '' AND bl.GodownName IN (SELECT GodownName FROM #tmpSourceGodownName)  OR (@GodownName_Source_List = ''  AND bl.GodownName = bl.GodownName ))
+And (@GodownName_Destination_List  <> '' AND BL.DestinationGodownName IN (SELECT GodownName FROM #tmpDestinationGodownName)  OR (@GodownName_Destination_List = ''  AND BL.DestinationGodownName = BL.DestinationGodownName ))
 
 Order by bl.EntryDate Desc
-
-
-
-
-
-	--Select top(100) * from TD_Txn_BatchLine where VoucherId=202955
-	--Select top(100) * from TD_Txn_AccLine where VoucherId=202955
-	--Select top(100) * from TD_Txn_VchHdr where VoucherId=202955
-	--Select top(100) * from TD_Txn_AccLine where VoucherId=202955
-	--Select distinct VoucherType from TD_Mst_VoucherType
